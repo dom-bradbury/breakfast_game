aadc81b55e8ea7b939b3d8cb434dbdb72f3355e3
dombradbury <dombradbury@googlemail.com>
2018-02-24 16:30:53 +0000

    Improved flask project so it can handle variable

--- breakfast_game/flask_test.py
+++ breakfast_game/flask_test.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Your Secret Key'
+
 
 @app.route("/")
 def main():
-    print('Hello Dom')
+    print('executing main')
+    session['output'] = ''
     return render_template('test.html')
+
 
 @app.route('/', methods=['POST'])
 def read_input():
+    print('executing read_input')
     # read the posted values from the UI
     input = request.form['userinput']
-    print(input)
-    return render_template('test.html')
+    output = '<p>' + input + '</p>' + session.get('output')
+    session['output'] = output
+
+    with open('templates/test.html') as f:
+        print(f.read().format(output))
+
+    return render_template('test.html', output=output)
 
 if __name__ == "__main__":
-    print('Hello Again')
-    app.run()
+    app.run(debug=True)
