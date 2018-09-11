c1d3d7e1a095e931533cf8c1bdd04cc5a20e5910
dombradbury <dombradbury@googlemail.com>
2018-09-11 15:02:13 +0100

    Trying 10

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uwsgi uwsgi.ini
+web: python breakfast_game/app.py
--- breakfast_game/app.py
+++ breakfast_game/app.py
@@ -63,6 +63,6 @@ if __name__ == "__main__":
     game_process.start()
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
 
     print('Will I ever be executed?')
